22e31bf4b19d4ad6f0b3935bc95b806f9017a2cb
Ramiro Gonzalez <rgonzalez69@ucmerced.edu>
2019-04-12 22:50:49 -0700

    Create hello.asm

---
+++ Practice/hello.asm
@@ -0,0 +1,18 @@
+; This is my first comment. 
+; Ramiro Gonzalez 
+section .data 
+message db "Hello, Universe", 10 
+section .text 
+global _start 
+
+_start: 
+        mov rax, 1 
+        mov rdi, 1
+        mov rsi, message 
+        mov rdx, 14 ; Each character in the string is one byte. Add +1 to include nul \0 character
+        syscall 
+        
+        move rax, 60 ; 'exit' 
+        xor rdi, rdi 
+        syscall 
+        
